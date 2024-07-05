715e994b47db6a7a0abd1ec02fba332bd94bb437
Taein <hurri29@kakao.com>
2024-07-06 01:36:00 +0900

    2024년 7월 5일 5장 완료, 6장 학습중

---
+++ springboot-developer/src/main/java/me/taein/springbootdeveloper/domain/Article.java
@@ -0,0 +1,32 @@
+package me.taein.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter // 게터 자동 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 자동 생성
+public class Article {
+
+    @Id // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false) // NOT NULL
+    private String title;
+
+    @Column(name = "content", nullable = false) // NOT NULL
+    private String content;
+
+    @Builder // 빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+}
