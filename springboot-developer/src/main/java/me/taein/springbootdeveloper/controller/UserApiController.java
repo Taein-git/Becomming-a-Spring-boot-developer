85cc0285511c798962fe64141cf4ff58f417a0e7
Taein <hurri29@kakao.com>
2024-07-15 20:45:23 +0900

    2024년 7월 15일 8장 끝

---
+++ springboot-developer/src/main/java/me/taein/springbootdeveloper/controller/UserApiController.java
@@ -0,0 +1,29 @@
+package me.taein.springbootdeveloper.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import me.taein.springbootdeveloper.dto.AddUserRequest;
+import me.taein.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request) {
+        userService.save(request);  // 회원가입 메서드 호출
+        return "redirect:/login";   // 회원가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
+    }
+}
---
+++ springboot-developer/src/main/java/me/taein/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.taein.springbootdeveloper.repository;
+
+import me.taein.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);   // email로 사용자 정보를 가져옴
+
+}
